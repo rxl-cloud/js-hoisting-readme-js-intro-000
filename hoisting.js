4f0c2f1001c76960da022160388ece94949f4b8a
Ruoxin Lin <ruoxinlin.ltg@gmail.com>
2019-07-25 01:42:35 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,5 +1,6 @@
+var lyric = "maybe";
 function callMe() {
-    var lyric = "maybe";
+
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
